d5cd5cd0c1630e7b02db0675ac1ccba25dedff35
mraaru11 <71057545+mraaru11@users.noreply.github.com>
2021-12-22 21:23:35 +0530

    cpp

---
+++ sieveOfEratosthenes.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void primeSilve(int n){
+
+    int prime[100] = {0};
+
+    for(int i=2; i<=n; i++){
+        if(prime[i]==0){
+            for(int j=i*i;j<=n;j+=i){
+                prime[j]=1;
+            }
+        }
+    }
+    for(int i=2; i<=n; i++){
+        if(prime[i]==0){
+            cout<<i<<" ";
+        }
+    }cout<<endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    primeSilve(n);
+    return 0;
+}
