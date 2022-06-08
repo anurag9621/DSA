1a35bb91eaf65d7c4f70f4560a302ca6a168f401
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-08 20:26:16 +0530

    k

---
+++ array/finding-dubli-sorted.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int lastDublicate = 0;
+    int j=0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] == arr[i + 1] && arr[i] != lastDublicate)
+        {
+            lastDublicate = arr[i];
+            cout << arr[i] << " ";
+            
+        }
+    }
+    return 0;
+}
