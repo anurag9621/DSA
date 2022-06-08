492b303492b60048c84ff36291301c6d51c63282
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-08 20:02:56 +0530

    add

---
+++ array/more-then-onemissing-element.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int l = arr[0];
+    int h = arr[n - 1];
+
+    int diff = l;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] - i != diff)
+        {
+            while (diff < arr[i] - i)
+            {
+                cout << i + diff << " ";
+                diff++;
+            }
+        }
+    }
+
+    return 0;
+}
