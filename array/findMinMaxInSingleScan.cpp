90595dc2186d45cd38605e930e4f2e61f36ef78f
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-10 20:42:46 +0530

    com

---
+++ array/findMinMaxInSingleScan.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int min= arr[0];
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+        else if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    cout<<"min: "<<min<<" max: "<<max;
+
+    return 0;
+}
