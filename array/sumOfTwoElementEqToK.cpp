6f99e518b624ebd0e0d2c10b540647fd325db462
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-10 20:23:47 +0530

    yo

---
+++ array/sumOfTwoElementEqToK.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == k)
+            {
+                cout << arr[i] << " + " << arr[j] << " = " << k << endl;
+            }
+        }
+    }
+
+    return 0;
+}
