df24569dd6c09805595234f50afd4f7d5193122c
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-15 23:15:54 +0530

    ok

---
+++ strings/changingCaseOpp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    char A[]="wElCoMe";
+    int i;
+    for(i=0;A[i]!='\0';i++){
+        if(A[i]>=65 && A[i]<=90){
+            A[i]=A[i]+32;
+        }
+        else if (A[i]>='a' && A[i]<='z')
+        {
+            A[i]-=32;
+        }
+        
+    }
+    cout<<A<<endl;
+}
