5fe9e35db1807aef8673f4dac0c939a7dd9a0a8b
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-17 12:48:04 +0530

    for now

--- strings/validateAstring.cpp
+++ strings/validateAstring.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int valid(chae *name){
+int valid(char * name){
     int i;
     for(i=0;name[i]!='\0';i++){
-        
+        if(!(name[i]>=65&&name[i]<=90)&&!(name[i]>=97&&name[i]<=122)&&!(name[i]>=48&&name[i]<=57)){
+            return 0;
+        }
     }
+    return 1;
 }
 
 
 int main()
 {
-    char *name="Anurag321";
+    char * name="Anurag321";
+    if(valid(name)){
+        cout<<"valid input"<<endl;
+    }else{
+        cout<<"invalid input"<<endl;
+    }
     return 0;
 }
