eab2fb1ce98b93012f1cdd44fb2ea3544eeb4e4d
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-17 12:12:27 +0530

    ok

--- strings/counting.cpp
+++ strings/counting.cpp
@@ -1,10 +1,15 @@
-#include<iosteam>
+#include <iostream>
 using namespace std;
 
-int main(){
-    char A[]="How are you";
-    int i,count=0;
-    for(i=0;A[i]!='\0';i++){
-        
+int main()
+{
+    char A[] = "How are you";
+    int i, count = 0;
+    for (i = 0; A[i] != '\0'; i++)
+    {
+        if (A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' || A[i] == 'A' || A[i] == 'E' || A[i] == 'I' || A[i] == 'O' || A[i] == 'U'){
+            count++;
+        }
     }
+    cout<<count;
 }
