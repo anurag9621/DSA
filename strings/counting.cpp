8cae92d0d90bbaf9a994a9bd63f55829033df5c4
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-17 12:14:30 +0530

    oj

--- strings/counting.cpp
+++ strings/counting.cpp
@@ -10,6 +10,9 @@ int main()
         if (A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' || A[i] == 'A' || A[i] == 'E' || A[i] == 'I' || A[i] == 'O' || A[i] == 'U'){
             count++;
         }
+        else if((A[i]>=65&&A[i]<=90)||(A[i]>=97&&A[i]<=122)){
+            
+        }
     }
     cout<<count;
 }
