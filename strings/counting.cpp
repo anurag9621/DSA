08c431be0a4c6f347094de4cb9e879f6d6cb5eea
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-17 12:21:38 +0530

    pp

--- strings/counting.cpp
+++ strings/counting.cpp
@@ -4,15 +4,20 @@ using namespace std;
 int main()
 {
     char A[] = "How are you";
-    int i, count = 0;
+    int i;
+    int vcount=0;
+    int ccount = 0;
     for (i = 0; A[i] != '\0'; i++)
     {
         if (A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u' || A[i] == 'A' || A[i] == 'E' || A[i] == 'I' || A[i] == 'O' || A[i] == 'U'){
-            count++;
+            vcount++;
         }
-        else if((A[i]>=65&&A[i]<=90)||(A[i]>=97&&A[i]<=122)){
-            
+        else if ((A[i] >= 'A' && A[i] <= 'Z') || (A[i] >= 'a' && A[i] <= 'z'))
+        {
+            ccount++;
         }
     }
-    cout<<count;
+    cout<<vcount<<endl;
+    cout<<ccount<<endl;
+
 }
