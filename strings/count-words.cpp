445545180eae91c3273a8d64f927175fe81e2dea
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-17 12:26:30 +0530

    ok

---
+++ strings/count-words.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char A[]="How are you";
+    int words=1;
+    for(int i=0;A[i]!='\0';i++){
+    if(A[i]==' '){
+        words++;
+    }
+    }
+    cout<<words<<endl;
+    return 0;
+}
