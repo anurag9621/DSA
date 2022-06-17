416ecf4211f1c2f2b1c91bef72624a2c79c94196
Anurag Pandey <arushpandey123buy@gmail.com>
2022-06-17 12:34:17 +0530

    ok

--- strings/count-words.cpp
+++ strings/count-words.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main()
 {
-    char A[]="How are you";
-    int words=1;
-    for(int i=0;A[i]!='\0';i++){
-    if(A[i]==' '&&A[i-1]!=' '){
-        words++;
+    char A[] = "How are you";
+    int words = 1;
+    for (int i = 0; A[i] != '\0'; i++)
+    {
+        if (A[i] == ' ' && A[i - 1] != ' ')
+        {
+            words++;
+        }
     }
-    }
-    cout<<words<<endl;
+    cout << words << endl;
     return 0;
 }
