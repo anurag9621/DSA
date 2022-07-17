a2b0db92845ea889674f0e428f8f60cad5d3d5e1
Anurag Pandey <arushpandey123buy@gmail.com>
2022-07-17 05:53:59 +0530

    ok

---
+++ linkedlist/inserting.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+} *first = NULL;
+
+void insert(int pos, int x)
+{
+    Node *t, *p;
+    if (pos == 0)
+    {
+        t = new Node;
+        t->data = x;
+        t->next = first;
+        first = t;
+    }
+    else if (pos > 0)
+    {
+        p = first;
+        for (int i = 0; i < pos - 1 && p; i++)
+        {
+            p = p->next;
+        }
+        if (p)
+        {
+            t = new Node;
+            t->data = x;
+            t->next = p->next;
+            p->next = t;
+        }
+    }
+}
+
+void display(Node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    insert(0, 10);
+    insert(1,20);
+    insert(1, 30);
+    display(first);
+}
