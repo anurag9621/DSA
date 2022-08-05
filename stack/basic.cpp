5d7599f2440694b2268a7b758a9c08fe04091b76
Anurag Pandey <arushpandey123buy@gmail.com>
2022-08-05 22:52:30 +0530

    ok

--- stack/basic.cpp
+++ stack/basic.cpp
@@ -1,11 +1,64 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-struct Stack{
-    
+
+class Stack
+{
+public:
+    int *arr;
+    int top;
+    int size;
+
+    Stack(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        top = -1;
+    }
+    void push(int x){
+        if(top==size-1){
+            cout << "Stack is Overflow";
+        }
+        else{
+            top++;
+            arr[top] = x;
+        }
+    }
+    void pop(){
+        if(top>=0){
+            top--;
+        }
+        else{
+            cout << "Stack Underflow";
+        }
+    }
+    int peek(){
+        if(top>=0){
+            return arr[top];
+        }
+        else{
+            cout << "Stack is empty";
+            return -1;
+        }
+    }
+    bool isEmpty(){
+        if(top==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
 };
+
 int main()
 {
-
-
+    Stack st(5);
+    st.push(22);
+    st.push(44);
+    st.push(20);
+    cout<<st.peek()<<endl;
+    cout << st.isEmpty();
     return 0;
 }
