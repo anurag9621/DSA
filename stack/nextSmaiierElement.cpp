275ee01a21c896279fac7aa9a65ed40922f685a4
Anurag Pandey <arushpandey123buy@gmail.com>
2022-08-10 21:49:59 +0530

    ok

---
+++ stack/nextSmaiierElement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<int> s;
+    s.push(-1);
+    vector<int> and;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        
+    }
+
+    return 0;
+}
---
+++ stack/validcom.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    stack<char> s;
+    char A[] = "{{}}";
+    if(A.length()%2!=o){
+        cout << -1;
+    }
+    else{
+        for (int i = 0; A[i] != '\0';i++){
+            char ch = A[i];
+            if(ch="{")
+            {
+                s.push(ch);
+            }
+            else{
+                if()
+            }
+        }
+    }
+
+    return 0;
+}
