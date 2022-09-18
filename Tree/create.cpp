aefa8da8adef892d6ba8ab6928ddd0bbc035e74b
Anurag Pandey <arushpandey123buy@gmail.com>
2022-09-19 04:35:36 +0530

    all

--- Tree/create.cpp
+++ Tree/create.cpp
@@ -10,6 +10,22 @@ struct Node {
         left = right = NULL;
     }
 };
+void preOrder(Node){
+    if(Node ==NULL){
+        return;
+    }
+    cout << Node->data;
+    preOrder(Node->left);
+    preOrder(Node->right);
+}
+void inOrder(Node){
+    if(Node==NULL){
+        return;
+    }
+    inOrder(Node->left);
+    cout << Node->data;
+    inOrder(Node->right);
+}
 int main(){
     struct Node *root = new Node(2);
     return 0;
